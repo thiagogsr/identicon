57659fbd4e6d0dd056ce40602b3e856ce9abadd6
Thiago Guimarães <thiagogsr@gmail.com>
2017-08-22 10:27:59 -0300

    Generates a md5 hash for the input

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,18 @@
 defmodule Identicon do
   @moduledoc """
-  Documentation for Identicon.
+    Generate an unique avatar for an input.
   """
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Identicon.hello
-      :world
-
+    Calls every needed methods to generate an image from a input.
   """
-  def hello do
-    :world
+  def main(input) do
+    input
+    |> hash_input(input)
+  end
+
+  defp hash_input(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
   end
 end
