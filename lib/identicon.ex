29a58c90231ab3c0cba3fb24fd73ce1d0c4bad7b
Thiago Guimarães <thiagogsr@gmail.com>
2017-08-22 13:57:24 -0300

    Filters odd squares and updates image grid with the result

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -11,6 +11,15 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build_grid
+    |> filter_odd_squares
+  end
+
+  defp filter_odd_squares(%Identicon.Image{grid: grid} = image) do
+    grid = Enum.filter grid, fn({code, _index}) ->
+      rem(code, 2) == 0
+    end
+
+    %Identicon.Image{image | grid: grid}
   end
 
   defp build_grid(%Identicon.Image{hex: hex} = image) do
