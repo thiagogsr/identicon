d1eaa2677db2e8408273508a2e418ba2fd200eb7
Thiago Guimarães <thiagogsr@gmail.com>
2017-08-22 11:00:59 -0300

    Builds a struct for identicon image with hex attribute

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -8,11 +8,13 @@ defmodule Identicon do
   """
   def main(input) do
     input
-    |> hash_input(input)
+    |> hash_input
   end
 
   defp hash_input(input) do
-    :crypto.hash(:md5, input)
+    hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
   end
 end
