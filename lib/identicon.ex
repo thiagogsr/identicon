6519505f6275aa9337b521b51ec8cae476eed4c9
Thiago Guimarães <thiagogsr@gmail.com>
2017-08-22 12:22:25 -0300

    Builds grid with hash list

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -10,6 +10,18 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
+  end
+
+  defp build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
+    |> Enum.map(&mirror_row/1)
+  end
+
+  defp mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
   end
 
   defp pick_color(%Identicon.Image{hex: [red, green, blue | _tail]} = image) do
